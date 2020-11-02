4dd7a418aa771435eae88535dc6a459e7d6303aa
Charles JABBOUR GEDEON <c.jg@zerocodeautomation.io>
2020-11-02 14:31:23 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'introduction';
+  // public title = 'introduction';
+  // public subtitle = 'how to code with angular';
+
+  public title = undefined;
+  public text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, quo?';
+  public color = 'red';
+  public imagepath = 'https://media-exp1.licdn.com/dms/image/C560BAQHMnA03XDdf3w/company-logo_200_200/0?e=1612396800&v=beta&t=S9-bYCNGo75A5Q2Q3qzId2H5l4Vt7t-FuJJT1rzOtwE';
+  public hidden = false;
+  public users = new Array<User>();
+
+
+  constructor() {
+    setTimeout(() => {
+      this.title = {
+        main: 'introduction',
+        subtitle: 'how to code with angular'
+      }
+    }, 0);
+    this.users.push(new User("john", "microsoft"))
+  }
+
+  public addUser(
+    event: MouseEvent,
+    p1: string, p2: string
+  ) {
+    this.users.push(new User(p1, p2));
+
+  }
+
+
 }
